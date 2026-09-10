d111e7f83067672bde5e38a10c8a9d0ad21594c8
RichyKunBv <reepito03@gmail.com>
2026-09-09 22:58:24 -0600

    ahora si el icono

--- test_drag.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Avalonia.Input;
-class Program {
-    static void Main() {
-        foreach(var prop in typeof(DragEventArgs).GetProperties()) {
-            Console.WriteLine(prop.Name);
-        }
-    }
-}
