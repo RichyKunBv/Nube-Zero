d575e5d0be3ae6f7067bb1a5f4a5995f78b1878e
RichyKunBv <reepito03@gmail.com>
2026-09-10 19:01:01 -0600

    0.2.2

--- src/shared/Version.cs
+++ src/shared/Version.cs
@@ -2,6 +2,6 @@ namespace NubeZero.Shared
 {
     public static class AppVersion
     {
-        public static string Texto { get; set; } = "v0.2.1";
+        public static string Texto { get; set; } = "v0.2.2";
     }
 }
