056cd0b7de322f53b006e3236d9c744a7663f1bb
RichyKunBv <reepito03@gmail.com>
2026-09-10 18:32:15 -0600

    cosas

--- src/server/Data/DatabaseContext.cs
+++ src/server/Data/DatabaseContext.cs
@@ -2,12 +2,7 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-#if NET472
-using Mono.Data.Sqlite;
-using SqliteConnection = Mono.Data.Sqlite.SqliteConnection;
-#else
 using Microsoft.Data.Sqlite;
-#endif
 namespace NubeZero.Server.Data
 {
     public class DatabaseContext
--- src/shared/Version.cs
+++ src/shared/Version.cs
@@ -2,6 +2,6 @@ namespace NubeZero.Shared
 {
     public static class AppVersion
     {
-        public static string Texto { get; set; } = "v0.1.6";
+        public static string Texto { get; set; } = "v0.1.7";
     }
 }
